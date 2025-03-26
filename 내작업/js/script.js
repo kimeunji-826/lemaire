2c0185533c31dc37ce00039efcc0c6935479a9fc
kimeunji-826 <with880826@gmail.com>
2025-03-26 11:19:21 +0900

    Delete 내작업 directory

--- 내작업/js/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener(`DOMContentLoaded`, function () {
-  const menuWoman = document.querySelector(`.menu1`);
-  menuWoman.addEventListener(`mouseenter`, function () {
-    this.classList.toggle(`open`);
-  });
-});
